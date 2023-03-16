10435b20577d85d55a6f5f99f20186983efa3ed4
Diniru Sandipa <harsha.ranepura@digiratina.com>
2023-03-16 11:01:16 +0530

    vehicle add, read, update vehicle and remove vehicle features added with valications

---
+++ backend/models/vehicle.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const VehicleSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    make: { type: String },
+    makeHide: { type: Boolean, default: false },
+    model: { type: String },
+    modelHide: { type: Boolean, default: false },
+    plateNo: { type: String },
+    plateNoHide: { type: Boolean, default: false },
+    passengers: { type: Number },
+    passengersHide: { type: Boolean, default: false },
+    registered: { type: Boolean, default: false },
+    registeredHide: { type: Boolean, default: false },
+    vehicleType: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Vehicle = new mongoose.model("vehicles", VehicleSchema);
+
+module.exports = Vehicle;
