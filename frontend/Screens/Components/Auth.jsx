4b5b2f323e4a09cc39684709cec2bfc82f14a335
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2023-03-25 02:56:27 +0530

    update navigation in user management

--- frontend/Screens/Components/Auth.jsx
+++ frontend/Screens/Components/Auth.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import Login from "./UserManagement/auth/Login";
-import Login from "./UserManagement/UserProfile";
+import Login from "./UserManagement/auth/Login";
+// import Login from "./UserManagement/UserProfile";
 import Register from "./UserManagement/auth/Register";
 
 const AuthNavigationStack = () => {
--- frontend/Screens/Components/User.jsx
+++ frontend/Screens/Components/User.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserProfile from "./UserManagement/UserProfile";
+import UpdateUser from "./UserManagement/UpdateUser";
 
 const UserNavigationStack = () => {
   const Stack = createNativeStackNavigator();
@@ -12,6 +13,7 @@ const UserNavigationStack = () => {
       }}
     >
       <Stack.Screen name="UserProfile" component={UserProfile} />
+      <Stack.Screen name="UpdateUser" component={UpdateUser} />
     </Stack.Navigator>
   );
 };
