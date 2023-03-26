34d70d087121c668233d0136a74e2e361cd667f6
SulakshanaGeeth <sulakshanageeth@gmail.com>
2023-03-26 13:26:01 +0530

    minor bug fix in backend.

--- backend/server.js
+++ backend/server.js
@@ -57,3 +57,4 @@ app.use("/incident", require("./routes/incident.route"));
 app.use("/user", require("./routes/user.route"));
 app.use("/vehicle", require("./routes/vehicle.route"));
 app.use("/user", require("./routes/user.route"));
+app.use("/request", require("./routes/request.route"));
